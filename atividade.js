ac7fc2b385c01ddcad20d9dfaf1ffe1ef9ff2079
Mizuicy <102737167+Mizuicy@users.noreply.github.com>
2022-05-12 09:34:57 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,38 @@
+///////////////////////////////PRIMEIRA ATIVIDADE 😎😋
+
+let numero = prompt ('Acerte o numero de 0 a 5 que estou pensado DUVIDO😎😎');
+
+if (numero == 2) {
+    alert('Opa, Ahh,acertou')
+} else {
+    alert('Opa, vc Errou');
+}
+//////////////////////////////SEGUNDA ATIVIDADE 😃🤣
+let velocidade = prompt ('Atenção! digite aqui a sua velocidade em km/h')
+if (velocidade > 80){
+    let multa = (velocidade-80)*5
+    alert(`Atenção! vc está multado ${multa}`)
+
+}else{
+    alert('continua sua viagem')
+}
+//////////////////////////////TERCEIRA ATIVIDADE 🤐😐
+
+
+let opa = prompt ('Digita Qualquer Numero ai ')
+
+if (numero %2 === 0){
+    alert("isso é par")
+}else{
+    alert("isso é impar")
+}
+
+//////////////////////////////////QUARTA ATIVIDADE 🥰😆
+let salario = parseInt (prompt('digite o seu salario'))
+if (salario > 1250){
+    let aumento = (salario *10/100)+salario;
+    alert(aumento);
+}else{
+    let aumento = (salario *15/100)+salario;
+    alert(aumento)
+}
